da51ec4622da8b50a9054a41a99405ba060947f7
MrRav3n <dawidmrraven@gmail.com>
2019-10-19 16:46:30 +0200

    Updates

---
+++ precache-manifest.b0239690a4b0eaecef56c58b03ec40df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "275c65b77f55a87d9c87",
+    "url": "/Crypto-Colors/static/js/runtime~main.275c65b7.js"
+  },
+  {
+    "revision": "2cd6894aadeb6dbc4993",
+    "url": "/Crypto-Colors/static/js/main.2cd6894a.chunk.js"
+  },
+  {
+    "revision": "e2ec55a0feee391301cc",
+    "url": "/Crypto-Colors/static/js/1.e2ec55a0.chunk.js"
+  },
+  {
+    "revision": "2cd6894aadeb6dbc4993",
+    "url": "/Crypto-Colors/static/css/main.e662fedd.chunk.css"
+  },
+  {
+    "revision": "e2ec55a0feee391301cc",
+    "url": "/Crypto-Colors/static/css/1.baff7280.chunk.css"
+  },
+  {
+    "revision": "e593a50b6c265660b0fee3de4409573b",
+    "url": "/Crypto-Colors/index.html"
+  }
+];
