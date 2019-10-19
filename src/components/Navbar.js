8de56ef14b287054e713ad789cae4882e3c8033a
MrRav3n <dawidmrraven@gmail.com>
2019-10-19 17:21:54 +0200

    fix bug (back to main page button)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import './App.css'
-
+import {
+    Link
+} from "react-router-dom";
 import React from 'react'
     //Navbar with account address
     function Navbar({account}) {
             return(
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container">
-                        <a className="navbar-brand text-center" href="index.html"><h2>Crypto-Colors</h2></a>
+                        <Link className="navbar-brand text-center" to="/"><h2>Crypto-Colors</h2></Link>
                         <span className="text-white ml-auto ">
                             <h4 className="text-white d-none d-lg-block">Your account: {account} </h4>
                         </span>
